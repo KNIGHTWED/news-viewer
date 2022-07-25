302871316a22836610e8e76d234409ebf531c311
Jeongmin Kim <jminkim@tecace.com>
2022-07-25 17:35:51 +0900

    not fixed

---
+++ src/components/NewsList.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px){
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  };
+`;
+
+const NewsList = ( category ) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [qcategory, setQCategory] = useState('');
+
+  useEffect(() => {
+    // async 사용 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'all' ? setQCategory('') : setQCategory(category);
+        console.log('주소: '+ `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=887b66f4f0ca475696ca309844b1273b`);
+
+        const response = await axios.get(
+          // 현재는 카테고리 선택이 없어진 것 같음
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=887b66f4f0ca475696ca309844b1273b`,
+          // `https://newsapi.org/v2/top-headlines?country=kr&apiKey=887b66f4f0ca475696ca309844b1273b`,
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+  if(loading) {
+    return <NewsListBlock>Loading...</NewsListBlock>;
+  }
+
+  if(!articles){
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
